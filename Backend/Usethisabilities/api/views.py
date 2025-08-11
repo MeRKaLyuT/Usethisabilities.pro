c0c5ca132a7d52d9727cdf575674bf75c450a9a2
Merkalyut <merkalyutme@gmail.com>
2025-08-12 01:36:38 +0300

    move server test commands to api app

---
+++ Backend/Usethisabilities/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def ping(_request):
+    return Response({'ok': True})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    return Response({'id': request.user.id, 'username': request.user.username})
