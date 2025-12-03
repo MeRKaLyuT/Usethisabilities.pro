84be87091fd756e033da3d2b8962fd5225c93445
Merkalyut <merkalyutme@gmail.com>
2025-12-03 22:32:59 +0300

    services.py: cookies responses

---
+++ Backend/Usethisabilities/accounts/services.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+from django.conf import settings
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def create_token(user):
+    refresh_t = RefreshToken.for_user(user)
+    access_t = refresh_t.access_token
+    return str(refresh_t), str(access_t)
+
+def set_auth_cookies(response, refresh_token: str, access_token: str):
+    # secure_flag = not settings.DEBUG       turn it on after the prod
+    cookie_params = {
+        "httponly": True,
+        #"secure": secure_flag,
+        "samesite": "Lax", # Lax if front on another domen (base = Strict)
+    }
+
+    response.set_cookie( # access
+        settings.AUTH_COOKIE_ACCESS,
+        access_token,
+        max_age = int(timedelta(minutes=120).total_seconds()),
+        **cookie_params,
+    )
+
+    response.set_cookie( # refresh
+        settings.AUTH_COOKIE_REFRESH,
+        refresh_token,
+        max_age= int(timedelta(days=30).total_seconds()),
+        **cookie_params,
+    )
+
+def clear_auth_cookies(response):
+    response.delete_cookie(settings.AUTH_COOKIE_ACCESS)
+    response.delete_cookie(settings.AUTH_COOKIE_REFRESH)
+    return response
--- Backend/Usethisabilities/accounts/views.py
+++ Backend/Usethisabilities/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ( ListView, DetailView, CreateView, UpdateView, DeleteView )
+
+
+
 
 # Create your views here.
