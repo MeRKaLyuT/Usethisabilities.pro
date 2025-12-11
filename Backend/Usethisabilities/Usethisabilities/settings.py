68509cf7e9b7917b44350679f4d0da0af4d0de0c
Merkalyut <merkalyutme@gmail.com>
2025-12-11 23:25:07 +0300

    feat: account +authentication code (because httponly = true)

--- Backend/Usethisabilities/Usethisabilities/settings.py
+++ Backend/Usethisabilities/Usethisabilities/settings.py
@@ -121,6 +121,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'accounts.authentication.CookieAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
