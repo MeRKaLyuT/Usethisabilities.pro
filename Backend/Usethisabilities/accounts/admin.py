1ef2c8d0c16040d3b62783e218bae2585111c1c9
Merkalyut <merkalyutme@gmail.com>
2025-12-24 11:00:29 +0300

    feat: extended admin panel

--- Backend/Usethisabilities/accounts/admin.py
+++ Backend/Usethisabilities/accounts/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("id", "email", "is_staff", "is_active")
+    ordering = ("email",)
+    search_fields = ("email")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_active", "is_staff"),
+        }),
+    )
+
 
 # Register your models here.
