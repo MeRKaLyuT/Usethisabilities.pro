fe128bfa2a6fb0998ff0c89463d8d1c82c9306a1
Merkalyut <merkalyutme@gmail.com>
2025-12-24 11:07:40 +0300

    feat: some small changes

--- Backend/Usethisabilities/accounts/admin.py
+++ Backend/Usethisabilities/accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
